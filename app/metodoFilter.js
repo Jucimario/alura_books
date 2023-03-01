ea6f8774ce23df2231b3d77019bdb9102a85fb66
jucimario <jucimarioo@live.com>
2023-03-01 17:12:55 -0300

    Incluindo Filtros

---
+++ app/metodoFilter.js
@@ -0,0 +1,10 @@
+const botoes = document.querySelectorAll('.btn');
+
+botoes.forEach(btn => btn.addEventListener('click', ()=>{
+   
+
+    let livrosFiltrados = livros.filter(livro => livro.categoria == btn.value);
+  
+    exibirLivros(livrosFiltrados);
+}));
+
