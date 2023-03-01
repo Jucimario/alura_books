d7a304c038fe86ea4c04bb904009e32198078f5c
jucimario <jucimarioo@live.com>
2023-03-01 17:13:20 -0300

    Acicionar Filtros

--- app/main.js
+++ app/main.js
@@ -6,7 +6,7 @@ getBusacarLivros(endPointDaApi);
 async function getBusacarLivros(endPointDaApi) {
 
     const response = await fetch(endPointDaApi);
-    const livros = await response.json();
+    livros = await response.json();
     
     //console.log(livros);
     let livrosComDesconto = aplicarDesconto(livros);
--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,6 +1,7 @@
 const elementoParaInserirLivros = document.getElementById('livros');
 
 function exibirLivros(livros) {
+  elementoParaInserirLivros.innerHTML = '';
     livros.forEach(livro => {
         elementoParaInserirLivros.innerHTML += `
         <div class="livro">
