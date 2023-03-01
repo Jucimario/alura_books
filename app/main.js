3fdb9ce21bb203b57117c378200394e916b3ca30
jucimario <jucimarioo@live.com>
2023-03-01 15:27:24 -0300

    Incluindo desconto e Refatorando codigo

--- app/main.js
+++ app/main.js
@@ -1,7 +1,5 @@
 let livros = [];
 const endPointDaApi ='https://guilhermeonrails.github.io/casadocodigo/livros.json';
-const elementoParaInserirLivros = document.getElementById('livros');
-
 
 getBusacarLivros(endPointDaApi);
 
@@ -10,25 +8,10 @@ async function getBusacarLivros(endPointDaApi) {
     const response = await fetch(endPointDaApi);
     const livros = await response.json();
     
-    console.table(livros);
+    //console.log(livros);
+    let livrosComDesconto = aplicarDesconto(livros);
 
-    exibirLivros(livros);
-}
+    //console.log(livrosComDesconto);
 
-function exibirLivros(livros) {
-    livros.forEach(livro => {
-        elementoParaInserirLivros.innerHTML += `
-        <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-        <h2 class="livro__titulo">
-          ${livro.titulo}
-        </h2>
-        <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">R$${livro.preco}</p>
-        <div class="tags">
-          <span class="tag">${livro.categoria}</span>
-        </div>
-      </div>
-        `;
-    });
+    exibirLivros(livrosComDesconto);
 }
